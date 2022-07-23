3f1174e37ae5c129f942be64c0319aa73a9e0479
nguyenquangtho6868 <105498060+nguyenquangtho6868@users.noreply.github.com>
2022-07-23 22:27:23 +0700

    Add files via upload

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -11,8 +11,9 @@ function ExpenseItem(props) {
     <Card className=" expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <div className="expense-item__price">{props.id}</div>
       </div>
       <button onClick={clickHandler}>Change title</button>
     </Card>
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,7 +1,7 @@
 import ExpenseItem from "./ExpenseItem";
 import Card from "./Card";
 import "./Expenses.css";
-import ExpenseFilter from "./ExpenseItem/ExpenseFilter";
+import ExpenseFilter from "./ExpenseFilter/ExpenseFilter";
 import { useState } from "react";
 
 function Expenses(props) {
@@ -9,33 +9,27 @@ function Expenses(props) {
   const filterChangeHandler = (selectedYear) => {
     setfilteresYaer(selectedYear);
   };
+  const filteredExpense = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+  let DieuKien = <p>Khong co gi</p>;
+  if (filteredExpense.length > 0) {
+    DieuKien = filteredExpense.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
   return (
     <Card className="expenses">
       <ExpenseFilter
         selected={filteredYear}
         onChangeFiter={filterChangeHandler}
       />
-
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+      {DieuKien}
     </Card>
   );
 }
